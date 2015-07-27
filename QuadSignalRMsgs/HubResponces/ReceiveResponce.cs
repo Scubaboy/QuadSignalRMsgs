73488364cdf34cd59da1537a723720fa17a4d9df
Scubaboy <robin@robincawsey.plus.com>
2015-07-27 07:33:50 +0100

    Added signalr receive responce message.

---
+++ QuadSignalRMsgs/HubResponces/ReceiveResponce.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadSignalRMsgs.HubResponces
+{
+    public class ReceiveResponce
+    {
+        private readonly bool msgProcessed;
+
+        public ReceiveResponce(bool msgProcessed)
+        {
+            this.msgProcessed = msgProcessed;
+        }
+
+        public bool MsgProcessed
+        {
+            get
+            {
+                return this.msgProcessed;
+            }
+        }
+    }
+}
