fe9444d65104c993733de5b99e0b7a2703f98e3a
Scubaboy <robin@robincawsey.plus.com>
2015-08-18 07:02:28 +0100

    Made hub to client responce generic.

--- QuadSignalRMsgs/HubResponces/ReceiveResponce.cs
+++ QuadSignalRMsgs/HubResponces/ReceiveResponce.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QuadSignalRMsgs.HubResponces
+﻿namespace QuadSignalRMsgs.HubResponces
 {
+    /// <summary>
+    /// Notifies a client that the hub has processed the clients message.
+    /// </summary>
     public class ReceiveResponce
     {
+        /// <summary>
+        /// Indicates that the message has been processed.
+        /// </summary>
         private readonly bool msgProcessed;
 
+        /// <summary>
+        /// ReceiveResponce constructor
+        /// </summary>
+        /// <param name="msgProcessed">Message process status.</param>
         public ReceiveResponce(bool msgProcessed)
         {
             this.msgProcessed = msgProcessed;
         }
 
+        /// <summary>
+        /// Gets the message process status.
+        /// </summary>
         public bool MsgProcessed
         {
             get
---
+++ QuadSignalRMsgs/HubResponces/Responce.cs
@@ -0,0 +1,52 @@
+﻿
+namespace QuadSignalRMsgs.HubResponces
+{
+    /// <summary>
+    /// Hub to client responce
+    /// </summary>
+    public class Responce
+    {
+        /// <summary>
+        /// Responce to be sent to the quad
+        /// </summary>
+        private byte[] responce;
+        
+        /// <summary>
+        /// Id of quad responce is intented for.
+        /// </summary>
+        private int quadId;
+        
+        /// <summary>
+        /// Creates a hub to quad responce.
+        /// </summary>
+        /// <param name="responce">Responce</param>
+        /// <param name="quadId">Id of quad to receive the responce</param>
+        public Responce(byte[] responce, int quadId)
+        {
+            this.responce = responce;
+            this.quadId = quadId;
+        }
+
+        /// <summary>
+        /// Gets the responce
+        /// </summary>
+        public byte[] Responce
+        {
+            get
+            {
+                return this.responce;
+            }
+        }
+
+        /// <summary>
+        /// Gtes the quad Id.
+        /// </summary>
+        public int QuadId
+        {
+            get
+            {
+                return this.quadId;
+            }
+        }
+    }
+}
