bb89aaf52c0f73fa90b65295e64ad40b44391783
Scubaboy <robin@robincawsey.plus.com>
2015-08-24 08:09:13 +0100

    Added ackrequired to Responce class.

--- QuadSignalRMsgs/HubResponces/Responce.cs
+++ QuadSignalRMsgs/HubResponces/Responce.cs
@@ -15,16 +15,34 @@ namespace QuadSignalRMsgs.HubResponces
         /// Id of quad responce is intented for.
         /// </summary>
         private int quadId;
-        
+
+        /// <summary>
+        /// Quad needs to send an ack to acknowledge receipt of message.
+        /// </summary>
+        private bool ackRequired;
+
         /// <summary>
         /// Creates a hub to quad responce.
         /// </summary>
         /// <param name="responce">Responce</param>
         /// <param name="quadId">Id of quad to receive the responce</param>
-        public Responce(byte[] responce, int quadId)
+        /// <param name="ackRequired">Quad needs to send an ack to acknowledge receipt of message.</param>
+        public Responce(byte[] responce, int quadId, bool ackRequired)
         {
             this.responce = responce;
             this.quadId = quadId;
+            this.ackRequired = ackRequired;
+        }
+
+        /// <summary>
+        /// Gets the message ack status.
+        /// </summary>
+        public bool AckRequired
+        {
+            get
+            {
+                return this.ackRequired;
+            }
         }
 
         /// <summary>
